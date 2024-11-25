881ea83fd2dcc9c8ed1d6154f83c42cb14ebbccd
Franco Capiglioni <fcapiglioni@uoc.edu>
2024-11-25 15:30:14 +0100

    1

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
