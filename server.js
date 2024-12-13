5c0191290e6aba9850ed770408218e339d79a569
Franco Capiglioni <fcapiglioni@uoc.edu>
2024-12-13 02:02:22 +0100

    probably finished

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const cors = require('cors');
