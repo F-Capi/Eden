29ac0a91cb50b1e658a0847d420874ea354fcdbd
Franco Capiglioni <fcapiglioni@uoc.edu>
2024-12-31 17:04:04 +0100

    almost done

--- public/js/router.js
+++ public/js/router.js
@@ -9,7 +9,6 @@ const toggleNumberingAndInfo = (show) => {
 
 const loadPage = async (page, projectTitle = '') => {
 
-
     if ((page === 'home' || page === '') && window.matchMedia('(max-width: 700px)').matches) {
         page = 'work';
     }
@@ -58,9 +57,6 @@ const loadPage = async (page, projectTitle = '') => {
             resetLinksBackgrounds();
             document.getElementById("index").classList.add("active-link");
             contentDiv.innerHTML = await fetch('/partials/projectPage.html').then(res => res.text());
-            if (window.getSelection) {
-                window.getSelection().removeAllRanges();
-            }
             await loadScript('/js/project-page.js');
             break;
         default:
